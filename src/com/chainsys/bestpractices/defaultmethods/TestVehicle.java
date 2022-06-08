3733f92bba06ba5378e094a5172d369ab459da6a
Deepa145 <Deepa.ravi@chainsys.com>
2022-06-08 10:32:09 +0530

    adding new

--- src/com/chainsys/bestpractices/defaultmethods/TestVehicle.java
+++ src/com/chainsys/bestpractices/defaultmethods/TestVehicle.java
@@ -2,14 +2,14 @@ package com.chainsys.bestpractices.defaultmethods;
 
 public class TestVehicle {
 
-	public static void main(String[] args) {
-Car firstCar = new Car();
-firstCar.start();
-firstCar.move();
+public static void main(String[] args) {
+		
+		Car firstCar = new Car();
+		firstCar.start();
+		firstCar.move();
 
-Bike firstBike = new Bike();
-firstBike.start();
-firstBike.move();
-	}
-
+		Bike firstBike = new Bike();
+		firstBike.start();
+		firstBike.move();
+}
 }
